3870167278d2010bbb73af06c91238d81c05974a
Ines Rodrigues <iim@sfu.ca>
2025-08-07 13:03:03 -0700

    added delete player option + separated start game from add player

--- server/game_manager.py
+++ server/game_manager.py
@@ -8,29 +8,36 @@ SUITS = ['♠', '♥', '♦', '♣']
 class Game_Manager:
     def __init__(self):
         self.deck = []
-        self.setup_deck()
         self.players = []
         self.player_count = 0
         self.add_player() #Dealer
         self.dealer = self.players[0]
-          
-    def setup_deck(self):
-        self.deck.clear()
-        for rank in RANKS:
-            for suit in SUITS:
-                card = rank + suit
-                self.deck.append(card)
         
-        random.shuffle(self.deck)
-    
+        self.active_players = []
+          
     #Handle events
-    def handle_new_player(self):
-        player = self.add_player()
-        return [player.hand, self.dealer.hand[0]]
+    def start_game(self):
+        self.setup_deck()
+        
+        game_info = []
+        for player in self.players:
+            self.active_players.append(player)
+            self.deal_card(player.playerID, 2)
+            game_info.append((player.playerID, player.hand, player.score))
+        return game_info
+    
+    def reset_game(self):
+        self.setup_deck()
+        self.current_turn_index = 1
+        for player in self.players:
+            player.reset()
+            self.deal_card(player.playerID, 2)
     
     def handle_hit(self, playerID):
-        player = self.get_player_from_playerID(playerID)
-        card = self.deal_card(playerID)
+        player = self.players[playerID]
+        card = self.deal_card(player.playerID, 1)
+        if player.is_bust:
+            self.handle_stand()
         return [card, player.score, player.is_bust]
     
     #def handle_stand(self): 
@@ -41,7 +48,6 @@ class Game_Manager:
         player = Player(self.player_count)
         self.player_count += 1
         self.players.append(player)
-        self.deal_card(player.playerID, 2)
         return player
         
     def deal_card(self, playerID, number_of_cards):
@@ -59,12 +65,6 @@ class Game_Manager:
     def play_dealer_turn(self):
         while self.dealer.score < 17:
             self.deal_card(self.dealer.playerID, 1)
-    
-    def reset_game(self):
-        self.setup_deck()
-        for player in self.players:
-            player.reset()
-            self.deal_card(player.playerID, 2)
         
     #Helpers
     def get_player_from_playerID(self, playerID):
@@ -72,5 +72,22 @@ class Game_Manager:
             if player.playerID == playerID:
                 return player
         return None
+    
+    def delete_player(self, playerID):
+        player = self.get_player_from_playerID(playerID)
+        if player:
+            self.players.remove(player)
+            self.player_count -= 1
+            return True
+        return False
+    
+    def setup_deck(self):
+        self.deck.clear()
+        for rank in RANKS:
+            for suit in SUITS:
+                card = rank + suit
+                self.deck.append(card)
+        
+        random.shuffle(self.deck)
         
     
--- server/main.py
+++ server/main.py
@@ -5,16 +5,17 @@ def main():
     game = Game_Manager()
     
     #game setup
-    game.handle_new_player()
-    for i in range(game.player_count):
-        p = game.players[i]
+    game.add_player()
+    game_info = game.start_game()
+    
+    for i, player_info in enumerate(game_info):
         if i == 0:
-            print(f"dealer: {p.hand} with score {p.score}")
+            print(f"dealer: {player_info[1]} with score {player_info[2]}")
         else:
-            print(f"player {p.playerID}: {p.hand} with score {p.score}")
+            print(f"player {player_info[0]}: {player_info[1]} with score {player_info[2]}")
     
     #game simulation (player hit + dealer)
-    game.deal_card(1, 1)
+    game.handle_hit(1)
     print(f"player {game.players[1].playerID}: {game.players[1].hand} with score {game.players[1].score}")
     game.play_dealer_turn()
     print(f"dealer: {game.players[0].hand} with score {game.players[0].score}")
@@ -30,6 +31,10 @@ def main():
     game.reset_game()
     print(f"dealer: {game.players[0].hand} with score {game.players[0].score}")
     print(f"player {game.players[1].playerID}: {game.players[1].hand} with score {game.players[1].score}")
+    
+    print(f"\nplayer count: {game.player_count}, players: {[p.playerID for p in game.players]}")
+    game.delete_player(game.players[1].playerID)
+    print(f"player count: {game.player_count}, players: {[p.playerID for p in game.players]}")
     
     
 if __name__ == "__main__":
